8c34f384bd03fb3f643a6ed8a91b334a698c10b7
Junior <nog.junior84@gmail.com>
2016-04-27 20:58:44 -0300

    Add authentication to application

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.all.order('created_at DESC')
@@ -9,11 +10,11 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = current_user.posts.build
+    @post = Post.new
   end
 
   def create
-    @post = current_user.posts.build(post_params)
+    @post = Post.new(post_params)
 
     if @post.save
       redirect_to @post
